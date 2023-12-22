2eebd6ea6e67e06dff79752f60c568fb32961c08
baobluebird <lebaohv1236bqt@gmail.com>
2023-12-22 12:02:13 +0700

    update

--- src/models/TrackModel.js
+++ src/models/TrackModel.js
@@ -6,14 +6,11 @@ const trackSchema = new mongoose.Schema(
         user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
         genres: { type: String ,required: true},
         singer: { type: String ,required: true},
-<<<<<<< HEAD
         description: {
             type: String,
             required: true
         },
-=======
         description: { type: String ,required: true},
->>>>>>> e4eb2bc10e34137c364ecdbdb19509b745db5d89
         image: { data: Buffer, contentType: String },
         music: { data: Buffer, contentType: String },
     },
